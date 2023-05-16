6eb435d5dc46ce0185f7c5440707b1633c61807f
gendloop <gendloop@163.com>
2023-05-16 08:58:21 +0800

    chore: push current changes

--- Step1/1.2/tree.ps1
+++ Step1/1.2/tree.ps1
@@ -5,5 +5,5 @@ $exist = Test-Path tree.txt
 if ($exist -eq $true) {
     Remove-Item -Path tree.txt
 }
-git-bash.exe -c "tree -L 3 -F -I tree --dirs-first > tree.txt"
+git-bash.exe -c "tree -L 3 -F -I 'tree|build' --dirs-first > tree.txt"
 popd
