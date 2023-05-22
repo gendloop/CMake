a3b7cee0dc51425ed2b57ff289be525fb139157a
gendloop <gendloop@163.com>
2023-05-22 23:00:04 +0800

    chore: push current changes

---
+++ Introduction/CMakeTutorial/Step5/5.1/build.ps1
@@ -0,0 +1,19 @@
+$exist = Test-Path build
+if ($exist -eq $true) {
+    #Remove-Item -Path build/** -Recurse
+}
+else {
+    New-Item -Path . -Name .\build -ItemType Directory
+}
+
+Push-Location build
+
+# use math
+Write-Host "define USE_MATH" -ForegroundColor Green
+cmake .. -DUSE_MATH=ON
+cmake --build . --config Debug
+
+ctest -C Debug -N
+
+Pop-Location
+
---
+++ Introduction/CMakeTutorial/Step5/5.1/src/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+#include <config.h>
+
+// 3rdParty
+#ifdef USE_MATH
+    #include <MathFunctions.h>
+#endif
+
+int main(int argc, char* argv[]) {
+    using namespace std;
+
+    cout << argv[0] << endl;
+    cout << "VERSION_MAJOR: " << VERSION_MAJOR << endl;
+    cout << "VERSION_MINOR: " << VERSION_MINOR << endl;    
+    
+    if(argc < 2) {
+        return 0;
+    }
+
+    const double value = stod(argv[1]);
+#ifndef USE_MATH
+    cout << "sqrt value: " << sqrt(value) << endl;
+#else
+    cout << "sqrt value: " << mysqrt(value) << endl;
+#endif
+    return 0;
+}
