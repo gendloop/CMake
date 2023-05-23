97b8b9ac49e19cbff24f7d787f77b21e213c8c4d
gendloop <gendloop@163.com>
2023-05-23 23:02:25 +0800

    chore: update

---
+++ Introduction/CMakeTutorial/Step5/5.2/tree.ps1
@@ -0,0 +1,9 @@
+# tree
+$current_path = $PSScriptRoot
+pushd $current_path
+$exist = Test-Path tree.txt
+if ($exist -eq $true) {
+    Remove-Item -Path tree.txt
+}
+git-bash.exe -c "tree -L 3 -F -I 'tree|build' --dirs-first > tree.txt"
+popd
