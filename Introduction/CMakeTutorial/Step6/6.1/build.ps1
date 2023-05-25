5288d058d5a24fc1b350a0f0eb1d137526e91447
gendloop <gendloop@163.com>
2023-05-25 22:45:41 +0800

    feat: add `Introduction/CMakeTutorial/Step6`

---
+++ Introduction/CMakeTutorial/Step6/6.1/build.ps1
@@ -0,0 +1,22 @@
+$exist = Test-Path build
+if ($exist -eq $true) {
+    Remove-Item -Path build/** -Recurse
+}
+else {
+    New-Item -Path . -Name .\build -ItemType Directory
+}
+
+Push-Location build
+
+# use math
+Write-Host "define USE_MATH" -ForegroundColor Green
+cmake .. -DUSE_MATH=ON
+cmake --build . --config Debug
+cmake --build . --config Release
+
+ctest -C Debug -V
+
+cmake --install . --prefix "./install"
+
+Pop-Location
+
---
+++ Introduction/CMakeTutorial/Step6/6.2/src/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+#include <config.h>
+
+#include "a_table.h"
+
+// 3rdParty
+#ifdef USE_MATH
+    #include <MathFunctions.h>
+#endif
+
+int main(int argc, char* argv[]) {
+    using namespace std;
+
+    cout << argv[0] << endl;
+    cout << "VERSION_MAJOR: " << VERSION_MAJOR << endl;
+    cout << "VERSION_MINOR: " << VERSION_MINOR << endl;    
+    
+    if(argc < 2) {
+        return 0;
+    }
+
+    const double value = sqrtTable[0];
+    std::cout << "No matter what you input, it is always eq to 0" << std::endl;
+#ifndef USE_MATH
+    cout << "sqrt value: " << sqrt(value) << endl;
+#else
+    cout << "sqrt value: " << mysqrt(value) << endl;
+#endif
+    return 0;
+}
