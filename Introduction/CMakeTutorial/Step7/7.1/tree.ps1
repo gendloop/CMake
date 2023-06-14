353b949062727c114f3a4074d5cd1a7745330f0c
gendloop <gendloop@163.com>
2023-06-14 22:58:56 +0800

    chore: update

--- Introduction/CMakeTutorial/Step7/7.1/tree.ps1
+++ Introduction/CMakeTutorial/Step7/7.1/tree.ps1
@@ -1,9 +1,9 @@
 # tree
 $current_path = $PSScriptRoot
-pushd $current_path
+Push-Location $current_path
 $exist = Test-Path tree.txt
 if ($exist -eq $true) {
     Remove-Item -Path tree.txt
 }
-git-bash.exe -c "tree -L 3 -F -I 'tree|build|a_table.h' --dirs-first > tree.txt"
-popd
+git-bash.exe -c "tree -L 3 -F -I 'tree.txt|tree.ps1|build.ps1|build' --dirsfirst --charset utf-8 > tree.txt"
+Pop-Location
