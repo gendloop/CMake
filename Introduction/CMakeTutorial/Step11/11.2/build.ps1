63404058d5f044ba17eeb847f05e7e6f6320433c
gendloop <gendloop@163.com>
2023-10-25 17:46:07 +0800

    chore: update

---
+++ Introduction/CMakeTutorial/Step11/11.2/build.ps1
@@ -0,0 +1,14 @@
+if((Test-Path build) -eq $true) {
+    Remove-Item -Path build/** -Recurse
+}
+else {
+    New-Item -Path . -Name .\build -ItemType Directory
+}
+
+Push-Location build
+
+cmake ..
+cmake --build . --config Debug
+ctest -C Debug -VV 
+
+Pop-Location
